806984cfaf339bd19016d9e635d6932bd4a76800
Felicity <felicitymuhonja33@gmail.com>
2024-06-12 15:11:25 +0300

    Updated owner.py

--- models/owner.py
+++ models/owner.py
@@ -1,1 +1,23 @@
+from debug import cursor
 
+class Owner:
+    def __init__(self, name, address, phone_number, id=None):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+
+    def save(self):
+        sql = """
+            INSERT INTO owner (name, address, phone_number) VALUES (?, ?, ?)
+        """
+        cursor.execute(sql, (self.name, self.address, self.phone_number))
+        cursor.connection.commit()
+        self.id = cursor.lastrowid
+
+    def delete(self):
+        sql = """
+            DELETE FROM owners WHERE id = ?
+        """
+        cursor.execute(sql, (self.id,))
+        cursor.connection.commit()
