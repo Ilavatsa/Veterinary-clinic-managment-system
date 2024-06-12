6481f57102399d2ed6c53516256c14a8895a771f
Felicity <felicitymuhonja33@gmail.com>
2024-06-12 15:06:42 +0300

    Updated pet.py

---
+++ models/pet.py
@@ -0,0 +1,25 @@
+from debug import cursor
+
+class Pets:
+    def __init__(self, name, species, age, owner_id, vet_id, id=None):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.age = age
+        self.owner_id = owner_id
+        self.vet_id = vet_id
+
+    def save(self):
+        sql = """
+            INSERT INTO Pets (name, species, age, owner_id, vet-id) VALUES (?, ?, ?)
+        """
+        cursor.execute(sql, (self.name, self.species, self.age, self.owner_id, self.vet_id))
+        cursor.connection.commit()
+        self.id = cursor.lastrowid
+
+    def delete(self):
+        sql = """
+            DELETE FROM pet WHERE id = ?
+        """
+        cursor.execute(sql, (self.id,))
+        cursor.connection.commit()
