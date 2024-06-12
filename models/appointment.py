78df7a5be5fd43a66105b84330e58d66b77858bc
Felicity <felicitymuhonja33@gmail.com>
2024-06-12 21:46:26 +0300

    Update appointment.py

--- models/appointment.py
+++ models/appointment.py
@@ -1,24 +1,29 @@
-from debug import cursor
+from db_helper import execute_query, fetch
 
 class Appointment:
-    def __init__(self, date, time, pet_id, vet_id, id=None):
-        self.id = id
-        self.date = date
-        self.time = time
-        self.pet_id = pet_id
-        self.vet_id = vet_id
-
-    def save(self):
-        sql = """
-            INSERT INTO appointments (date, time, pet_id, vet_id) VALUES (?, ?, ?)
+    def create_table(self):
+        """Create the appointment table."""
+        query = """
+        CREATE TABLE IF NOT EXISTS appointment (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            pet_id INTEGER,
+            appointment_date TIMESTAMP,
+            status TEXT,
+            description TEXT,
+            FOREIGN KEY (pet_id) REFERENCES pet(id)
+        );
         """
-        cursor.execute(sql, (self.date, self.time, self.pet_id, self.vet_id))
-        cursor.connection.commit()
-        self.id = cursor.lastrowid
+        execute_query(query)
 
-    def delete(self):
-        sql = """
-            DELETE FROM appointments WHERE id = ?
+    def create(self, pet_id, appointment_date, status, description):
+        """Create a new appointment."""
+        query = f"""
+        INSERT INTO appointment (pet_id, appointment_date, status, description)
+        VALUES ({pet_id}, '{appointment_date}', '{status}', '{description}');
         """
-        cursor.execute(sql, (self.id,))
-        cursor.connection.commit()
+        execute_query(query)
+
+    def drop_table(self):
+        """Drop the appointment table."""
+        query = "DROP TABLE IF EXISTS appointment;"
+        execute_query(query)
