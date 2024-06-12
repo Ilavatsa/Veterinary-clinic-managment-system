b17e7ed213b35576b19046eaa52fe84501d8bb2d
Felicity <felicitymuhonja33@gmail.com>
2024-06-12 21:39:32 +0300

    Update setup_db.py

--- database/setup_db.py
+++ database/setup_db.py
@@ -1,15 +1,11 @@
-import sqlite3
+from db_helper import execute_query
 
 def setup_database():
-    conn = sqlite3.connect('vet_clinic.db')
-    cursor = conn.cursor()
-
-    with open('database/create_tables.sql', 'r') as sql_file:
-        sql_script = sql_file.read()
-    
-    cursor.executescript(sql_script)
-    conn.commit()
-    conn.close()
+    """Setup the database by creating tables."""
+    with open('create_tables.sql', 'r') as f:
+        sql_script = f.read()
+        execute_query(sql_script)
 
 if __name__ == "__main__":
     setup_database()
+
