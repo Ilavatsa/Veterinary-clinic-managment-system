e1a76cf8a6ba0fc1aeef10eb0981bc397992a7c7
Felicity <felicitymuhonja33@gmail.com>
2024-06-12 14:57:56 +0300

    Update Treatment.py

---
+++ models/treatment.py
@@ -0,0 +1,27 @@
+from debug import cursor
+
+class Treatment:
+    def __init__(self, description, cost, appointment_id, id=None):
+        self.id = id
+        self.description = description
+        self.cost = cost
+        self.appointment_id = appointment_id
+
+    def save(self):
+        sql = """
+            INSERT INTO treatment (description
+            ++
+            
+            
+            , cost, appointment_id) VALUES (?, ?, ?)
+        """
+        cursor.execute(sql, (self.description, self.cost, self.appointment_id))
+        cursor.connection.commit()
+        self.id = cursor.lastrowid
+
+    def delete(self):
+        sql = """
+            DELETE FROM medical_records WHERE id = ?
+        """
+        cursor.execute(sql, (self.id,))
+        cursor.connection.commit()
